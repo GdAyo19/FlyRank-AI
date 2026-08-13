f3c0d162ce8517f95260cc3cb9b4ba68a1dc5add
Ayodeji Olanrewaju <111335900+GdAyo19@users.noreply.github.com>
2026-08-13 10:09:30 +0100

    Delete CRUD API directory

--- CRUD API/repositories/__init__.py
+++
@@ -1,9 +0,0 @@
-from repositories.base import TaskRepository
-from repositories.sqlite_repository import SqliteTaskRepository
-from repositories.postgres_repository import PostgresTaskRepository
-
-__all__ = [
-    "TaskRepository",
-    "SqliteTaskRepository",
-    "PostgresTaskRepository",
-]
